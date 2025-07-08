833a16cbf2418ac0522eaf5e74d57e822dd87b58
heshanmadv <157682942+heshanmadv@users.noreply.github.com>
2025-07-08 21:51:15 +0530

    add some dependancy

--- user-service/src/main/java/com/levelup/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/levelup/user_service/UserServiceApplication.java
@@ -2,7 +2,7 @@ package com.levelup.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 public class UserServiceApplication {
