31b76eeec7980cff522bab0d990f649292c014ec
Suraja Hasarinda <surajahasarinda@gmail.com>
2025-07-11 16:36:23 +0530

    Implement User methods

---
+++ user-service/src/main/java/com/levelup/user_service/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.levelup.user_service.controller;
+
+import com.levelup.user_service.dto.UserDTO;
+import com.levelup.user_service.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable String userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<String> updateUser(@RequestBody UserDTO userDTO, @PathVariable String userId, @RequestHeader("X-User-ID") String currentUserId) {
+        return ResponseEntity.ok(userService.updateUser(userDTO, userId, currentUserId));
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable String userId, @RequestHeader("X-User-ID") String currentUserId) {
+        return ResponseEntity.ok(userService.deleteUser(userId, currentUserId));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserDTO> getCurrentUser(@RequestHeader("X-User-ID") String userId) {
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+
+}
--- user-service/src/main/java/com/levelup/user_service/dto/UserDTO.java
+++ user-service/src/main/java/com/levelup/user_service/dto/UserDTO.java
@@ -1,11 +1,13 @@
 package com.levelup.user_service.dto;
 
 import com.levelup.user_service.model.Role;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@Builder
 public class UserDTO {
     private String id;
     private String firstName;
