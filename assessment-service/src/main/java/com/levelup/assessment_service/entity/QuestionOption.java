45a674ad09eb8b96d9844678fbb25c380116df96
KasunyaH <kasunyahimani@gmail.com>
2025-07-16 14:30:14 +0530

    entity files for assesment service

---
+++ assessment-service/src/main/java/com/levelup/assessment_service/entity/QuestionOption.java
@@ -0,0 +1,41 @@
+package com.levelup.assessment_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "quiz_options")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionOption {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+    
+    @Column(name = "option_text", columnDefinition = "TEXT", nullable = false)
+    private String optionText;
+    
+    @Column(name = "is_correct", nullable = false)
+    private Boolean isCorrect; // Indicates if this option is the correct answer
+    
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+} 
