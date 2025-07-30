15218cbc1b208ac97e55b16add49b199cf85be65
heshanmadv <157682942+heshanmadv@users.noreply.github.com>
2025-07-30 19:35:32 +0530

    Add @Repository annotation to LessonRepository and update MongoDB database name in application.yml

--- course-service/src/main/java/com/levelup/course_service/repository/LessonRepository.java
+++ course-service/src/main/java/com/levelup/course_service/repository/LessonRepository.java
@@ -4,7 +4,9 @@ import com.levelup.course_service.model.Lesson;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LessonRepository extends MongoRepository<Lesson, String> {
     List<Lesson> findByCourseId(String courseId);
 }
