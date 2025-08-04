71d7ba7ed369298faae53d490dad37caaf49085f
heshanmadv <157682942+heshanmadv@users.noreply.github.com>
2025-08-04 22:29:01 +0530

    update course and lesson

--- api-gateway/src/main/java/com/levelup/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/levelup/api_gateway/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/api/auth/**", "/api/users/**", "/api/courses/**").permitAll()
+                        .pathMatchers("/api/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .build();
