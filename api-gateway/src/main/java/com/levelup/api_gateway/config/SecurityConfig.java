8364704158270c4ea98b6d5a2aba585bbf2e0cf5
Suraja Hasarinda <surajahasarinda@gmail.com>
2025-09-02 03:46:48 +0530

    Modify api gateway

--- api-gateway/src/main/java/com/levelup/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/levelup/api_gateway/config/SecurityConfig.java
@@ -1,8 +1,15 @@
 package com.levelup.api_gateway.config;
 
+import com.levelup.api_gateway.filter.JwtAuthenticationFilter;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,16 +20,32 @@ import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((exchange, ex) -> {
+                            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                            return exchange.getResponse().setComplete();
+                        })
+                )
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/api/**").permitAll()
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .pathMatchers("/api/auth/**").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/api/courses/**").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/api/instructors/**").permitAll()
                         .anyExchange().authenticated()
                 )
+                .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .addFilterBefore(corsWebFilter(), SecurityWebFiltersOrder.CORS)
                 .build();
     }
 
