d2b8febc1deffaab85fbf077d31a591d38d457b3
Suraja Hasarinda <surajahasarinda@gmail.com>
2025-09-02 10:51:56 +0530

    Minor changes

--- api-gateway/src/main/java/com/levelup/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/levelup/api_gateway/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.levelup.api_gateway.config;
 
 import com.levelup.api_gateway.filter.JwtAuthenticationFilter;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
