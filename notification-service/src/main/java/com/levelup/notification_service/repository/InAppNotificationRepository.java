82c7a3fc288ae3e1c6363994035dd74b72679ab7
KasunyaH <kasunyahimani@gmail.com>
2025-09-06 11:23:15 +0530

    notification tests

--- notification-service/src/main/java/com/levelup/notification_service/repository/InAppNotificationRepository.java
+++ notification-service/src/main/java/com/levelup/notification_service/repository/InAppNotificationRepository.java
@@ -1,8 +1,12 @@
 package com.levelup.notification_service.repository;
 
 import com.levelup.notification_service.entity.InAppNotification;
+import com.levelup.notification_service.entity.InAppType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.UUID;
 
 public interface InAppNotificationRepository extends JpaRepository<InAppNotification, UUID> {
+    List<InAppNotification> findByType(InAppType type);
+    List<InAppNotification> findByRead(boolean read);
 }
